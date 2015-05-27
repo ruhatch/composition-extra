6174bb527c3dca31f91566e38ecd67ae5579386a
Athan Clark <athan.clark@gmail.com>
2015-05-27 17:54:19 -0600

    asterisk-compatible, readme

---
+++ Readme.md
@@ -0,0 +1,56 @@
+composition-extra
+=================
+
+> Some handy tools for working with algebraic structures.
+
+## Functors
+
+We can `fmap` more deeply with Applicative-style syntax:
+
+```haskell
+(+1) <$$> [Just 1, Nothing, Just 2]
+> [Just 2, Nothing, Just 3]
+```
+
+Likewise, via [contravariant](http://hackage.haskell.org/package/contravariant)
+functors:
+
+```haskell
+:t (-2) <-$$$> (Predicate $ (==) Predicate $ (==) Predicate $ (==) 5)
+> :: ( Eq b
+     , Eq (Predicate b)
+     , Eq (Predicate (Predicate b))
+     , Num b
+     , Num (a -> Predicate b)
+     , Contravariant ((->) (Predicate (Predicate b) -> Bool))
+     ) => Predicate (Predicate a)
+```
+
+> (it's not that useful)
+
+## Functions
+
+You can slyly prepend unanry functions to arbitrary arguments (contravariant function
+compositon):
+
+```haskell
+before :: q -> d
+func2 :: c -> d -> e
+func3 :: b -> c -> d -> e
+func4 :: a -> b -> c -> d -> e
+
+before -.* func2 :: c -> q -> e
+before -.** func3 :: b -> c -> q -> e
+before -.*** func4 :: a -> b -> c -> q -> e
+```
+
+## Monads
+
+We can also do weird `liftM`-ish, applicative-y stuff, too:
+
+```haskell
+import Data.Compositon -- from the `compositon` package
+
+(return .: (+)) ==<< mx =<< my
+mx >>==== (return .:: func4) -- apply to 4th arg
+```
--- src/Data/Function/Contravariant/Syntax.hs
+++ src/Data/Function/Contravariant/Syntax.hs
@@ -10,37 +10,53 @@ module Data.Function.Contravariant.Syntax where
       -> a -> b -> d
 (-.:) p q a b = q a $ p b
 
+(-.*) = (-.:)
+
 (-.:.) :: (c -> d)
        -> (a -> b -> d -> e)
        -> a -> b -> c -> e
 (-.:.) p q a b c = q a b $ p c
+
+(-.**) = (-.:.)
 
 (-.::) :: (d -> e)
        -> (a -> b -> c -> e -> f)
        -> a -> b -> c -> d -> f
 (-.::) p q a b c d = q a b c $ p d
 
+(-.***) = (-.::)
+
 (-.::.) :: (e -> f)
         -> (a -> b -> c -> d -> f -> g)
         -> a -> b -> c -> d -> e -> g
 (-.::.) p q a b c d e = q a b c d $ p e
+
+(-.****) = (-.::.)
 
 (-.:::) :: (f -> g)
         -> (a -> b -> c -> d -> e -> g -> h)
         -> a -> b -> c -> d -> e -> f -> h
 (-.:::) p q a b c d e f = q a b c d e $ p f
 
+(-.*****) = (-.:::)
+
 (-.:::.) :: (g -> h)
          -> (a -> b -> c -> d -> e -> f -> h -> i)
          -> a -> b -> c -> d -> e -> f -> g -> i
 (-.:::.) p q a b c d e f g = q a b c d e f $ p g
+
+(-.******) = (-.:::.)
 
 (-.::::) :: (h -> i)
          -> (a -> b -> c -> d -> e -> f -> g -> i -> j)
          -> a -> b -> c -> d -> e -> f -> g -> h -> j
 (-.::::) p q a b c d e f g h = q a b c d e f g $ p h
 
+(-.*******) = (-.::::)
+
 (-.::::.) :: (i -> j)
           -> (a -> b -> c -> d -> e -> f -> g -> h -> j -> k)
           -> a -> b -> c -> d -> e -> f -> g -> h -> i -> k
 (-.::::.) p q a b c d e f g h i = q a b c d e f g h $ p i
+
+(-.********) = (-.::::.)
