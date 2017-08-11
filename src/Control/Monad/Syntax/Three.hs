d4e06b60fda95a4609e3963285111bac9235a100
Rupert Horlick <rupert@myrtlesoftware.com>
2017-08-11 10:36:22 +0100

    Add (>>.==) to Control.Monad.Syntax.Three

--- src/Control/Monad/Syntax/Three.hs
+++ src/Control/Monad/Syntax/Three.hs
@@ -1,26 +1,37 @@
 module Control.Monad.Syntax.Three where
 
-
-(===<<) :: Monad m =>
-           (a -> b -> c -> m d)
-        -> m a
-        -> b -> c -> m d
+(===<<)
+  :: Monad m
+  => (a -> b -> c -> m d)
+  -> m a
+  -> b -> c -> m d
 (===<<) mf inp b c = inp >>= (\a -> mf a b c)
 
 infixl 1 ===<<
 
-(=.=<<) :: Monad m =>
-           (a -> b -> c -> m d)
-        -> m b
-        -> a -> c -> m d
+(=.=<<)
+  :: Monad m
+  => (a -> b -> c -> m d)
+  -> m b
+  -> a -> c -> m d
 (=.=<<) mf inp a c = inp >>= (\b -> mf a b c)
 
 infixl 1 =.=<<
 
-(==.<<) :: Monad m =>
-           (a -> b -> c -> m d)
-        -> m c
-        -> a -> b -> m d
+(==.<<)
+  :: Monad m
+  => (a -> b -> c -> m d)
+  -> m c
+  -> a -> b -> m d
 (==.<<) mf inp a b = inp >>= mf a b
 
 infixl 1 ==.<<
+
+(>>.==)
+  :: Monad m
+  => m a
+  -> (a -> b -> c -> m d)
+  -> b -> c -> m d
+(>>.==) inp mf b c = inp >>= (\a -> mf a b c)
+
+infixr 2 >>.==
